fee4dbbee0f89dc21d0cdcd72664b4ace968c2cc
timur <gabbasowt@yandex.ru>
2024-10-23 11:13:42 +0300

    (fixed title) Solution: N

--- N.java
+++ N.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class ParkingLot {
+public class N {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
